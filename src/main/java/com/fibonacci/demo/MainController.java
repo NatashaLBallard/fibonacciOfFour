bb898e8b381a8e81359246e3ec6e4fb30318f952
NatashaLBallard <nbitcareer@gmail.com>
2018-01-29 12:02:15 -0500

    still working on loop; need to add a loop to continue and end it

--- src/main/java/com/fibonacci/demo/MainController.java
+++ src/main/java/com/fibonacci/demo/MainController.java
@@ -22,13 +22,35 @@ public class MainController {
         int num_1 = Integer.parseInt(number) - 1;
         int num_2 = Integer.parseInt(number) - 2;
         int num_3 = Integer.parseInt(number) - 3;
-
+        int num_4 = 0;
+        String storedNums = "";
 
         int fibNum = Integer.parseInt(number) + num_1 + num_2 + num_3;
+        String listOfFibNums = "";
+
+        do   {
+
+
+           // System.out.println(inputNum);
+            fibNum = Integer.parseInt(number) + num_1 + num_2 + num_3;
+
+            listOfFibNums += fibNum;
+
+            //inputNum = fibNum;
+            int newNum = num_4 + num_3;
+
+            num_1 = num_2;
+            num_2 = num_3;
+            num_3 = num_4;
+            num_4 = newNum;
+
+
+                 }
+        while (inputNum == fibNum);
 
 
 
-        return number + " " + fibNum;
+        return number + " " + fibNum + " " + listOfFibNums + "" + "(" + num_1 + " " + num_2 + " " + num_3 ;
 
     }
 
